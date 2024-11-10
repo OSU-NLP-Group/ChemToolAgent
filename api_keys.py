0b977b2a236a202a51424e543267f9bcdae8ea2a
btyu <btyu@foxmail.com>
2024-11-10 09:34:36 -0500

    add api sites

--- api_keys.py
+++ api_keys.py
@@ -1,10 +1,10 @@
 api_keys = {
     # LLMs
-    "OPENAI_API_KEY": "",
-    "ANTHROPIC_API_KEY": "",
+    "OPENAI_API_KEY": "",  # https://platform.openai.com/
+    "ANTHROPIC_API_KEY": "",  # https://console.anthropic.com/
 
     # Tools
-    "RXN4CHEM_API_KEY": "",
-    "CHEMSPACE_API_KEY": "",
-    "TAVILY_API_KEY": "",
+    "RXN4CHEM_API_KEY": "",  # https://rxn.app.accelerate.science/rxn
+    "CHEMSPACE_API_KEY": "",  # https://api.chem-space.com/docs/
+    "TAVILY_API_KEY": "",  # https://tavily.com/
 }
