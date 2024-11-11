2a5da1be3fdad78bb05b477b3a394d0c7a4ceb9b
btyu <btyu@foxmail.com>
2024-11-10 23:15:14 -0500

    upload webpage

---
+++ api_keys.py
@@ -0,0 +1,10 @@
+api_keys = {
+    # LLMs
+    "OPENAI_API_KEY": "",  # https://platform.openai.com/
+    "ANTHROPIC_API_KEY": "",  # https://console.anthropic.com/
+
+    # Tools
+    "RXN4CHEM_API_KEY": "",  # https://rxn.app.accelerate.science/rxn
+    "CHEMSPACE_API_KEY": "",  # https://api.chem-space.com/docs/
+    "TAVILY_API_KEY": "",  # https://tavily.com/
+}
